9d7dd16005154ee28c44f5061fae279496e77e37
Kaffekopp <ln222gu@student.lnu.se>
2013-12-21 16:26:39 -0500

    jsuppg5

--- valuta.js
+++ valuta.js
@@ -6,17 +6,22 @@ function konverteraValuta() {
     var kronor;
     var dollar = 0.15;
     var euro = 0.11;
-    kronor = 1;
+    kronor = document.getElementById("number").value;
     
-    svar = kronor * euro;
+    if (isNaN(kronor)) {
+        alert("FEEEEEEEEEEEEEEEEEEEEEEEEEEEL!");
+    }
+    else {
+    if (document.getElementById("valuta").value === "Euro") {
+        svar = kronor * euro;
+    }
+    else if (document.getElementById("valuta").value === "Dollar") {
+        svar = kronor * dollar;
+    }
     
     
-    
-    
-    
-    
-    
-    document.getElementById('answer').innerHTML="Liv är bäst.";
+    document.getElementById('answer').innerHTML=svar.toFixed(2);
+    }
 }
 
 var button = document.getElementById("konvertera");

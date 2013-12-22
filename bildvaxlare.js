0a77277105be7195e10364ec263b734a018146a7
Kaffekopp <ln222gu@student.lnu.se>
2013-12-22 14:02:31 -0500

    jsuppgiftklar

--- bildvaxlare.js
+++ bildvaxlare.js
@@ -1,5 +1,4 @@
 window.onload=function() {
-setInterval(function(){alert(bilder)},5000);
 
 var bilder = new Array();
 
@@ -7,11 +6,17 @@ bilder[0] = "http://fc00.deviantart.net/fs28/f/2008/049/5/7/Art_Nouveau_by_areve
 bilder[1] = "http://th08.deviantart.net/fs29/PRE/f/2008/123/0/1/WoW__Orc_hunter_by_areve.jpg";
 bilder[2] = "http://fc05.deviantart.net/fs43/f/2009/061/2/8/CHOW_149___Female_sith_lord_by_areve.jpg";
 
+var i = 0;
+setInterval(function(){
+    document.getElementById("bilder").setAttribute("src",bilder[i++]);
+    if (i === 3) {
+        i = 0;
+    }
+},5000);
 
 
 
 
-document.getElementById("bilder").setAttribute("src",bilder[1]);
 
 
 }
